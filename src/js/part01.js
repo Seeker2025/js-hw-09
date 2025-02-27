56ab47cfa203ad2febb5bf43b9fd5fe7f57d7202
Igor Yurovsky <85400723+Seeker2025@users.noreply.github.com>
2025-02-27 12:59:13 +0200

    go on

---
+++ src/js/part01.js
@@ -0,0 +1,6 @@
+
+//// Імпорт простору імен
+
+export const girl02 = 'Marichka';
+export const boy = 'Vasyl';
+export const boy02 = 'Petro';
---
+++ src/main.js
@@ -0,0 +1,11 @@
+
+
+
+import * as users from './js/part01';
+
+
+
+
+console.log(users.girl02);
+console.log(users.boy);
+console.log(users.boy02);
